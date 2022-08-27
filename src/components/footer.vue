8b207a70a23fbab396e72fbfa7bb0161ef264285
Роман <65034362+WEBSTER71@users.noreply.github.com>
2022-08-27 04:56:26 +0300

    lose tren popup

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -12,7 +12,7 @@
 				<div class="footer__bottom-box">
 					<a href="https://vk.com/websterov" class="footer__link">О нас</a>
 					<a href="https://vk.com/dimachka_5" class="footer__link">Вопросы и ответы</a>
-					<a href="tel:+79509000789" class="footer__link">+7-950-900-07-89</a>
+					<a href="tel:+7-950-900-07-89" class="footer__link">+7-950-900-07-89</a>
 					<a href="mailto:zhuludkovr@mail.ru" class="footer__link">zhuludkovr@mail.ru</a>
 				</div>
 			</div>
